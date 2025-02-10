e8b25dfb8542cb418734ede5fb856c900a79b015
alicfamahdi <alicfamahdi@gmail.com>
2025-02-10 23:48:52 +0700

    feat: initial development done

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -20,7 +20,7 @@ public class ProductController {
     public String createProductPage(Model model) {
         Product product = new Product();
         model.addAttribute("product", product);
-        return "createProduct";
+        return "CreateProduct";
     }
 
     @PostMapping("/create")
@@ -29,10 +29,10 @@ public class ProductController {
         return "redirect:list";
     }
 
-    @GetMapping("/create")
+    @GetMapping("/list")
     public String productListPage(Model model) {
         List<Product> allProducts = service.findAll();
         model.addAttribute("products", allProducts);
-        return "productList";
+        return "ListProduct";
     }
 }
