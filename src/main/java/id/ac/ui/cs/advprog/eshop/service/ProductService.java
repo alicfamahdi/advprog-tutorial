84790389585ebe088a878ca49042cdaba90ad0c8
alicfamahdi <alicfamahdi@gmail.com>
2025-02-13 13:44:39 +0700

    refactor: cleaner code

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,9 +4,9 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
-    public List<Product> findAll();
-    public void edit(String id, Product product);
-    public Product findById(String id);
-    public void delete(String productId);
+    public Product createProduct(Product product);
+    public List<Product> findAllProducts();
+    public void editProduct(String id, Product product);
+    public Product findProductById(String id);
+    public void deleteProduct(String productId);
 }
