024b19930d0b4e038d9cf42fec5687d3bb60eb61
alicfamahdi <alicfamahdi@gmail.com>
2025-02-14 19:05:43 +0700

    test: add CreateProductFunctionalTest

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomePageController.java
@@ -0,0 +1,18 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomePageController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "HomePage";
+    }
+}
