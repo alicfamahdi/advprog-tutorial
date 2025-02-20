9ac84ebf97dfdd1d1183c7388823b2f6cc922870
alicfamahdi <alicfamahdi@gmail.com>
2025-02-21 02:32:23 +0700

    fix: change koyeb port

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -2,11 +2,18 @@ package id.ac.ui.cs.advprog.eshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class EshopApplication {
+    private static final Logger logger = LoggerFactory.getLogger(EshopApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(EshopApplication.class, args);
+        String port = Optional.ofNullable(System.getenv("PORT")).orElse("8080");
+        logger.info("Application started on port: {}", port);
     }
-
 }
